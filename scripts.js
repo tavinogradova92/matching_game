95faf198515b1ad2db34b2129f31ac866defbb25
Tanja <tavinogradova92@gmail.com>
2019-11-22 15:03:16 +0100

    The right side without the last child added

--- scripts.js
+++ scripts.js
@@ -3,14 +3,22 @@ let numberOfFaces = 5;
 const theLeftSide = document.getElementById("leftSide");
 
 document.addEventListener('DOMContentLoaded', function generateFaces() {
-        for (i = 0; i < 5; i++) {
-        let img = document.createElement("img");
-        img.src = "smile.png";
-        img.style.top = `${Math.floor((Math.random() * 400) + 0)}px`;
-        img.style.left = `${Math.floor((Math.random() * 400) + 0)}px`;
+        //adding 5 random smiles to the left
+        for (i = 0; i < numberOfFaces; i++) {
+            let img = document.createElement("img");
+            img.src = "smile.png";
+            img.style.top = `${Math.floor((Math.random() * 400) + 0)}px`;
+            img.style.left = `${Math.floor((Math.random() * 350) + 0)}px`;
 
-        theLeftSide.appendChild(img);
-    };
+            theLeftSide.appendChild(img);
+        };
+
+        //handling the right side
+        const theRightSide = document.getElementById("rightSide");
+        let rightSideImages = theLeftSide.cloneNode(true);
+        rightSideImages.removeChild(rightSideImages.lastChild);
+        theRightSide.appendChild(rightSideImages);
+
 });
 
 console.log("Everything is working globally")
